b79a588adeb2222fadf5d07862c922f85383a4fe
Ismael Kentenich <ismaelkentenichm@gmail.com>
2026-08-14 18:10:00 -0300

    feat: update ESLint configuration and add vitest-axe for accessibility testing

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,13 @@
 import "@testing-library/jest-dom/vitest";
+import { expect } from "vitest";
+import type { AxeMatchers } from "vitest-axe/matchers";
+import * as matchers from "vitest-axe/matchers";
+
+expect.extend(matchers);
+
+declare module "vitest" {
+  /* eslint-disable @typescript-eslint/no-empty-object-type, @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any */
+  interface Assertion<T = any> extends AxeMatchers {}
+  interface AsymmetricMatchersContaining extends AxeMatchers {}
+  /* eslint-enable @typescript-eslint/no-empty-object-type, @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any */
+}
